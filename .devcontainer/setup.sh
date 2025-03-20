72fe78eb98dc48d1d0b0707d0698cd1572dc4e53
Hubert Szolc <szolc@agh.edu.pl>
2025-03-20 08:37:43 +0000

    fix: improve ROS_DOMAIN_ID validation in setup script

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
 set -e
 
-if [ $# -gt 0 ]; then
-    if [ $1 -gt 0 ]; then
-        echo "export ROS_DOMAIN_ID=$1" >> ~/.bashrc
-        export ROS_DOMAIN_ID=$1
-    fi
-else
-    echo "export ROS_DOMAIN_ID=0" >> ~/.bashrc
-    export ROS_DOMAIN_ID=0
+if [ $# -eq 0 ]; then
+    echo "Usage: $0 <ROS_DOMAIN_ID>"
+    exit 1
 fi
+
+if [ $1 -lt 0 ]; then
+    echo "ROS_DOMAIN_ID has to be a positive integer"
+    exit 1
+fi
+
+if [ $1 -gt 101 ]; then
+    echo "Maximum allowed value for ROS_DOMAIN_ID is 101"
+    exit 1
+fi
+
+echo "export ROS_DOMAIN_ID=$1" >> ~/.bashrc
+export ROS_DOMAIN_ID=$1
 echo "Using ROS_DOMAIN_ID=$ROS_DOMAIN_ID"
 
 cd /home/developer/ros2_ws
